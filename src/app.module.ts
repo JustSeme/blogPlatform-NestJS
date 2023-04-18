9894dfa5ba0e2de8e7d5217dfaec26eb8c4b0712
JustSemen <semyn03@mail.ru>
2023-04-18 19:38:36 +0300

    update imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,60 +1,61 @@
-import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
-import { UsersController } from './auth/api/users-controller'
-import { AuthService } from './auth/application/auth-service'
-import { EmailManager } from './general/managers/emailManager'
-import { UsersQueryRepository } from './auth/infrastructure/users-query-repository'
-import { MongooseModule } from '@nestjs/mongoose'
-import {
-  User, UsersSchema
-} from './auth/domain/UsersSchema'
-import { UsersRepository } from './auth/infrastructure/users-db-repository'
-import { BlogsService } from './blogs/application/blogs-service'
-import { BlogsQueryRepository } from './blogs/infrastructure/blogs/blogs-query-repository'
-import { BlogsRepository } from './blogs/infrastructure/blogs/blogs-db-repository'
-import { BlogsController } from './blogs/api/blogs-controller'
-import {
-  Blog, BlogSchema
-} from './blogs/domain/blogs/BlogsSchema'
-import {
-  Post, PostSchema
-} from './blogs/domain/posts/PostsSchema'
-import {
-  Comment, CommentsSchema
-} from './blogs/domain/comments/commentsSchema'
-import { PostsService } from './blogs/application/posts-service'
-import { PostsRepository } from './blogs/infrastructure/posts/posts-db-repository'
-import { JwtService } from './general/adapters/JwtService'
-import { PostsController } from './blogs/api/posts-controller'
-import { CommentsController } from './blogs/api/comments-controller'
-import { CommentsService } from './blogs/application/comments-service'
-import { CommentsQueryRepository } from './blogs/infrastructure/comments/comments-query-repository'
-import { CommentsRepository } from './blogs/infrastructure/comments/comments-db-repository'
-import { AuthController } from './auth/api/auth-controller'
-import { PassportModule } from '@nestjs/passport'
-import { LocalStrategy } from './auth/api/strategies/local.strategy'
-import { BasicStrategy } from './blogs/api/strategies/basic.strategy'
-import {
-  DeviceAuthSession, DeviceAuthSessionsSchema
-} from './security/domain/DeviceAuthSessionSchema'
-import { JwtStrategy } from './blogs/api/strategies/jwt.strategy'
-import { SecurityController } from './security/api/security-controller'
-import { SecurityService } from './security/application/security-service'
-import { DeviceRepository } from './security/infrastructure/device-db-repository'
-import {
-  ThrottlerGuard, ThrottlerModule
-} from '@nestjs/throttler'
-import { APP_GUARD } from '@nestjs/core'
-import { BcryptAdapter } from './general/adapters/BcryptAdapter'
-import { EmailAdapter } from './general/adapters/EmailAdapter'
+import { Module } from "@nestjs/common"
 import {
   ConfigModule, ConfigService
-} from '@nestjs/config'
-import { IsBlogByIdExist } from './general/decorators/isBlogExists.validation.decorator'
-import { IsEmailAlreadyInUse } from './auth/api/decorators/IsEmailAlreadyInUse'
-import { IsLoginAlreadyInUse } from './auth/api/decorators/IsLoginAlreadyInUse'
-import { IsDeviceExistsPipe } from './security/api/pipes/isDeviceExists.validation.pipe'
+} from "@nestjs/config"
+import { MongooseModule } from "@nestjs/mongoose"
+import {
+  ThrottlerGuard, ThrottlerModule
+} from "@nestjs/throttler"
+import {
+  User, UsersSchema
+} from "./auth/domain/UsersSchema"
+import {
+  Blog, BlogSchema
+} from "./blogs/domain/blogs/BlogsSchema"
+import {
+  Post, PostSchema
+} from "./blogs/domain/posts/PostsSchema"
+import {
+  Comment, CommentsSchema
+} from "./blogs/domain/comments/commentsSchema"
+import {
+  DeviceAuthSession, DeviceAuthSessionsSchema
+} from "./security/domain/DeviceAuthSessionSchema"
+import { PassportModule } from "@nestjs/passport"
+import { AppController } from "./app.controller"
+import { UsersController } from "./auth/api/users-controller"
+import { BlogsController } from "./blogs/api/blogs-controller"
+import { BlogsService } from './blogs/application/blogs-service'
+import { BlogsRepository } from './blogs/infrastructure/blogs/blogs-db-repository'
+import { PostsController } from "./blogs/api/posts-controller"
+import { CommentsController } from "./blogs/api/comments-controller"
+import { AuthController } from "./auth/api/auth-controller"
+import { SecurityController } from "./security/api/security-controller"
+import { LocalStrategy } from "./auth/api/strategies/local.strategy"
+import { JwtStrategy } from "./blogs/api/strategies/jwt.strategy"
+import { BasicStrategy } from "./blogs/api/strategies/basic.strategy"
+import { AppService } from "./app.service"
+import { AuthService } from "./auth/application/auth-service"
+import { JwtService } from "./general/adapters/JwtService"
+import { PostsService } from "./blogs/application/posts-service"
+import { CommentsService } from "./blogs/application/comments-service"
+import { SecurityService } from "./security/application/security-service"
+import { EmailManager } from "./general/managers/emailManager"
+import { BcryptAdapter } from "./general/adapters/BcryptAdapter"
+import { EmailAdapter } from "./general/adapters/EmailAdapter"
+import { BlogsQueryRepository } from "./blogs/infrastructure/blogs/blogs-query-repository"
+import { CommentsQueryRepository } from "./blogs/infrastructure/comments/comments-query-repository"
+import { UsersQueryRepository } from "./auth/infrastructure/users-query-repository"
+import { UsersRepository } from "./auth/infrastructure/users-db-repository"
+import { PostsRepository } from "./blogs/infrastructure/posts/posts-db-repository"
+import { CommentsRepository } from "./blogs/infrastructure/comments/comments-db-repository"
+import { DeviceRepository } from "./security/infrastructure/device-db-repository"
+import { IsBlogByIdExist } from "./general/decorators/isBlogExists.validation.decorator"
+import { IsEmailAlreadyInUse } from "./auth/api/decorators/IsEmailAlreadyInUse"
+import { IsLoginAlreadyInUse } from "./auth/api/decorators/IsLoginAlreadyInUse"
+import { IsDeviceExistsPipe } from "./security/api/pipes/isDeviceExists.validation.pipe"
+import { APP_GUARD } from "@nestjs/core"
+
 
 
 @Module({
