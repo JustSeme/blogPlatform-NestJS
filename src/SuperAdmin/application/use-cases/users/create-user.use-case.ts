21a43255982e2a94a61b15a04850e72514b20249
JustSemen <semyn03@mail.ru>
2023-07-18 14:09:18 +0300

    refactore create user use case transaction

--- src/SuperAdmin/application/use-cases/users/create-user.use-case.ts
+++ src/SuperAdmin/application/use-cases/users/create-user.use-case.ts
@@ -1,82 +1,70 @@
 import { BcryptAdapter } from "../../../../general/adapters/bcrypt.adapter"
-import {
-    CommandHandler, ICommandHandler
-} from "@nestjs/cqrs/dist"
+import { CommandHandler } from "@nestjs/cqrs/dist"
 import { FieldError } from "../../../../general/types/ErrorMessagesOutputModel"
 import { BadRequestException } from "@nestjs/common"
 import { UsersTypeORMRepository } from "../../../infrastructure/typeORM/users-typeORM-repository"
 import { InjectDataSource } from "@nestjs/typeorm"
-import { DataSource } from "typeorm"
+import {
+    DataSource, EntityManager
+} from "typeorm"
 import { UserEntity } from "../../../domain/typeORM/user.entity"
 import { UserBanInfo } from "../../../domain/typeORM/user-ban-info.entity"
 import { UserEmailConfirmation } from "../../../domain/typeORM/user-email-confirmation.entity"
 import { UserPasswordRecovery } from "../../../domain/typeORM/user-password-recovery.entity"
 import { AuthQueryTypeORMRepository } from "../../../../auth/infrastructure/typeORM/auth-query-typeORM-repository"
+import { TransactionBaseUseCase } from "../../../../general/use-cases/transaction-base.use-case"
 
 export class CreateUserCommand {
     constructor(public login: string, public password: string, public email: string) { }
 }
 
 @CommandHandler(CreateUserCommand)
-export class CreateUserUseCase implements ICommandHandler<CreateUserCommand> {
+export class CreateUserUseCase extends TransactionBaseUseCase<CreateUserCommand, string> {
     constructor(
-        private bcryptAdapter: BcryptAdapter,
-        private usersRepository: UsersTypeORMRepository,
-        private authQueryRepository: AuthQueryTypeORMRepository,
-        @InjectDataSource() private dataSource: DataSource,
-    ) { }
+        protected bcryptAdapter: BcryptAdapter,
+        protected usersRepository: UsersTypeORMRepository,
+        protected authQueryRepository: AuthQueryTypeORMRepository,
+        @InjectDataSource() protected dataSource: DataSource,
+    ) {
+        super(dataSource)
+    }
+
+    async doLogic(input: CreateUserCommand, manager: EntityManager): Promise<string> {
+        // if login or email is already in use - throw 400 error
+        await this.isEmailOrLoginAlreadyUsed(input.login, input.email)
+
+        const passwordHash = await this.bcryptAdapter.generatePasswordHash(input.password, 10)
+
+        const userEntityData = new UserEntity()
+        userEntityData.login = input.login
+        userEntityData.email = input.email
+        userEntityData.passwordHash = passwordHash
+        userEntityData.isConfirmed = true
+
+        const savedUser = await this.usersRepository.queryRunnerSave(userEntityData, manager)
+
+        const userBanInfoData = new UserBanInfo()
+        userBanInfoData.userId = userEntityData
+
+        await this.usersRepository.queryRunnerSave(userBanInfoData, manager)
+
+        const userEmailConfirmationData = new UserEmailConfirmation()
+        userEmailConfirmationData.emailConfirmationCode = null
+        userEmailConfirmationData.emailExpirationDate = null
+        userEmailConfirmationData.user = savedUser as UserEntity
+
+        await this.usersRepository.queryRunnerSave(userEmailConfirmationData, manager)
+
+        const userPasswordRecoveryData = new UserPasswordRecovery()
+        userPasswordRecoveryData.user = savedUser as UserEntity
+
+        await this.usersRepository.queryRunnerSave(userPasswordRecoveryData, manager)
+
+        return savedUser.id
+    }
 
     async execute(command: CreateUserCommand): Promise<string | null> {
-        // if login or email is already in use - throw 400 error
-        await this.isEmailOrLoginAlreadyUsed(command.login, command.email)
-
-        const passwordHash = await this.bcryptAdapter.generatePasswordHash(command.password, 10)
-
-        const queryRunner = this.dataSource.createQueryRunner()
-
-        await queryRunner.connect()
-
-        await queryRunner.startTransaction()
-
-        try {
-            const queryRunnerManager = queryRunner.manager
-
-            const userEntityData = new UserEntity()
-            userEntityData.login = command.login
-            userEntityData.email = command.email
-            userEntityData.passwordHash = passwordHash
-            userEntityData.isConfirmed = true
-
-            const savedUser = await this.usersRepository.queryRunnerSave(userEntityData, queryRunnerManager)
-
-            const userBanInfoData = new UserBanInfo()
-            userBanInfoData.userId = userEntityData
-
-            await this.usersRepository.queryRunnerSave(userBanInfoData, queryRunnerManager)
-
-            const userEmailConfirmationData = new UserEmailConfirmation()
-            userEmailConfirmationData.emailConfirmationCode = null
-            userEmailConfirmationData.emailExpirationDate = null
-            userEmailConfirmationData.user = savedUser as UserEntity
-
-            await this.usersRepository.queryRunnerSave(userEmailConfirmationData, queryRunnerManager)
-
-            const userPasswordRecoveryData = new UserPasswordRecovery()
-            userPasswordRecoveryData.user = savedUser as UserEntity
-
-            await this.usersRepository.queryRunnerSave(userPasswordRecoveryData, queryRunnerManager)
-
-            await queryRunner.commitTransaction()
-            return savedUser.id
-        } catch (err) {
-            console.error(err)
-
-            await queryRunner.rollbackTransaction()
-
-            return null
-        } finally {
-            await queryRunner.release()
-        }
+        return super.execute(command)
     }
 
     async isEmailOrLoginAlreadyUsed(login: string, email: string) {
